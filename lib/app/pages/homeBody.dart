3406143a6bcdb6ea04420d02abee59313547328d
naruesorn krabkhuntod <naruesorn.krabkhuntod@gmail.com>
2024-04-11 18:49:57 +0700

    dd

--- lib/app/pages/homeBody.dart
+++ lib/app/pages/homeBody.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class homeBody extends StatelessWidget {
   const homeBody({
     super.key,
@@ -9,19 +10,27 @@ class homeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30)),
-        color: Color(0xFFEDE4FF),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: const Color(0xFFA076F9),
       ),
-      width: MediaQuery.of(context).copyWith().size.width,
-      child: const Column(
-        children: [
-          Text('data'),
-          Text('data'),
-          Text('data'),
-          Text('data'),
-          Text('data'),
-        ],)
+      width: MediaQuery.of(context).copyWith().size.width * 0.85,
+      child: const Padding(
+        padding: EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('สรุปบัญชีรายวัน'),
+                Text('3,500 บาท', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),)
+              ],
+            ),
+            Spacer(),
+            Text('data'),
+          ],),
+      )
     );
   }
 }
